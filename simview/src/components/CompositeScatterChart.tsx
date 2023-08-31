3afa2ce332404b448ebd986b4c764f5be5bbbe35
Kate Martin <51387586+renanthera@users.noreply.github.com>
2023-08-30 20:09:38 -0600

    more changes to make it fit on one screen better. bearpray topple

--- simview/src/components/CompositeScatterChart.tsx
+++ simview/src/components/CompositeScatterChart.tsx
@@ -1,18 +1,16 @@
 'use client'
-import {VegaLite} from 'react-vega'
-import React, {useState} from 'react'
+import { VegaLite } from 'react-vega'
+import React, { useState } from 'react'
 
-import {all_sims, selected_sims} from '~/utils/ChartSchemas'
-import {PrettyPrintJSON} from '~/components/PrettyPrintJSON'
-import {useResizeObserver} from '~/components/useResizeObserver'
+import { all_sims, selected_sims } from '~/utils/ChartSchemas'
+import { PrettyPrintJSON } from '~/components/PrettyPrintJSON'
 
 
-export default function CompositeScatterChart({data}) {
+export default function CompositeScatterChart({ data }) {
   const actions = false
 
   const [all_sims_state, set_all_sims_state] = useState(null)
   const [selected_sims_state, set_selected_sims_state] = useState(null)
-  const {ref, width, height} = useResizeObserver()
 
   const signalListeners = {
     brush: (...args) => {
@@ -40,22 +38,18 @@ export default function CompositeScatterChart({data}) {
     }
   }
 
-  const style_1 = {
-    width: width,
-    height: height,
-  }
-
   return (
     <>
       <div className="grid grid-cols-[minmax(0,_1fr)_minmax(0,_2fr)] gap-4 h-[90%] max-h-[90%]">
-        {/* <div className="gap-4 h-[90%] [grid-template:_minmax(0,_1fr)_minmax(0,_2fr)_/_repeat(8,_1fr)]"> */}
-        <VegaLite className="w-full" spec={all_sims(data, style_1)} data={{'data': data}}
-                  signalListeners={signalListeners} actions={actions}/>
-        <div className="min-h-0 min-w-0 max-h-96 overflow-auto">
-          <PrettyPrintJSON data={selected_sims_state !== null ? selected_sims_state : all_sims_state}/>
+        <div className="flex flex-col h-[85vh] w-full">
+          <VegaLite className="h-1/2 w-full" spec={all_sims(data)} data={{ 'data': data }}
+            signalListeners={signalListeners} actions={actions} />
+          <VegaLite className="h-1/2 w-full" spec={selected_sims(all_sims_state)} data={{ 'data': all_sims_state }}
+            signalListeners={signalListeners_2} actions={actions} />
         </div>
-        <VegaLite className="w-full" spec={selected_sims(all_sims_state)} data={{'data': all_sims_state}}
-                  signalListeners={signalListeners_2} actions={actions}/>
+        <div className="min-h-0 min-w-0 max-h-[85vh] overflow-auto">
+          <PrettyPrintJSON data={selected_sims_state !== null ? selected_sims_state : all_sims_state} />
+        </div>
       </div>
     </>
   )
